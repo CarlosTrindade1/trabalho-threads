cffea0880d171aef531cc8b02192251a6d02e31a
Carlos Trindade <carlos.trindade@ufms.br>
2024-05-15 22:26:08 -0400

    Implementando a queue

--- include/queue.h
+++ include/queue.h
@@ -3,29 +3,38 @@
 
 #include <stddef.h>
 
+typedef struct node node_t;
+
 typedef struct node {
+   node_t* next;
+   int data;
 } node_t;
 
+typedef struct queue {
+   int size;
+   node_t* first;
+} queue_t;
+
 /* Initialize a queue */
-void queue_init(node_t * queue);
+void queue_init(queue_t** queue);
 
 /* Remove and return the item at the front of the queue Return NULL if the
    queue is empty */
-node_t *dequeue(node_t * queue);
+node_t *dequeue(queue_t* queue);
 
 /* Add item to the back of the queue */
-void enqueue(node_t * queue, node_t * item);
+void enqueue(queue_t* queue, node_t* item);
 
 /* Determine if the queue is empty.
  * Returns 1 if the queue is empty.
  * Returns 0 otherwise.
  */
-int is_empty(node_t *queue);
+int is_empty(queue_t* queue);
 
 /* Returns the first item in the queue
  * Returns NULL if the queue is empty
  */
-node_t *peek(node_t *queue);
+node_t *peek(queue_t* queue);
 
 /* A comparison function should return:
  *  1 if a is less-than-or-equal-to b;
--- lib/queue.c
+++ lib/queue.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <queue.h>
 
 /*
@@ -5,27 +6,68 @@
   manipulation. You  are free to  choose your own  implementation. You
   may need to define additional functions if you want to implement the
   extra-point functionality.
- */
-// TODO: inicializes a queue
-void queue_init(node_t * queue)
-{
+*/
+
+/*
+  @params: queue_t* queue: it's a pointer to the queue
+  @description: This function initializes a queue
+  @return: void
+*/
+void queue_init(queue_t** queue) {
+  *queue = (queue_t*)malloc(sizeof(queue_t));
+
+  // Check if it was possible to alloc memory
+  if (*queue == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  (*queue)->first = NULL;
+  (*queue)->size = 0;
 }
 
-// TODO: returns the first element of the queue
-node_t *dequeue(node_t * queue)
-{
-	return NULL;
+/*
+  @params: queue_t* queue: it's a pointer to the queue
+  @description: This function returns the first element in the queue
+  @return: node_t* first node: it returns the first node in the queue
+*/
+node_t* dequeue(queue_t* queue) {
+  // The queue is empty
+  if (queue->first == NULL) return NULL;
+
+  node_t* first_node = queue->first;
+
+  queue->first = first_node->next;
+
+  queue->size--;
+
+	return first_node;
 }
 
-// TODO: inserts a node in a queue
-void enqueue(node_t * queue, node_t * item)
-{
+/*
+  @params: 
+    queue_t* queue: it's a ponter to the queue
+    node_t* item: it's an element that will be added to the queue
+  @decription: This function adds an element to the end of the queue
+  @return: void
+*/
+void enqueue(queue_t* queue, node_t* item) {
+  if (queue->size == 0) {
+    queue->first = item;
+  } else {
+    node_t* node;
+
+    for (node = queue->first; node->next != NULL; node = node->next)
+      ;
+  
+    node->next = item;
+  }
+
+  queue->size++;
 }
 
 // TODO: checks if a queue is empty
-int is_empty(node_t *queue)
-{
-	return 0;
+int is_empty(queue_t* queue) {
+	return queue->size == 0;
 }
 
 
